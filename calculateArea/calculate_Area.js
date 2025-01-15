84c24e93a312e2819a3e4adc634429ac0c5d5530
emrekinci <emrekinci0794@gmail.com>
2025-01-15 15:57:42 -0500

    calculateGrocery

--- calculateArea/calculate_Area.js
+++ calculateArea/calculate_Area.js
@@ -8,3 +8,16 @@ function calculateArea() {
  let area = length * width;
  document.getElementById('result').innerText = `The area of the rectangle is: ${area}`;
 }
+
+let amount1;
+let amount2;
+let amount3;
+
+function calculateGrocery(){
+ amount1 = parseFloat(document.getElementById('amount-1').value); 
+ amount2 = parseFloat(document.getElementById('amount-2').value);
+ amount3 = parseFloat(document.getElementById('amount-3').value);
+
+ let totalAmount = amount1 + amount2 + amount3; 
+ document.getElementById('groceryTotal').innerText = `The Total Amount is : ${totalAmount}`;
+}
